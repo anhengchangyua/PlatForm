cadd9255a46ef16712312ec08f5fd2dab0ded433
anhengchangyua <188851312@qq.com>
2017-12-28 15:22:27 +0800

    add net util

--- app/src/main/java/com/zhy/uutils/hll_core/activities/ExampleActivity.java
+++ app/src/main/java/com/zhy/uutils/hll_core/activities/ExampleActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 
-import com.zhy.uutils.guolin16_canlendar.CanlendarDelegateT;
 import com.zhy.uutils.hll_core.delegates.HLDelegate;
 
 /**
@@ -26,7 +25,7 @@ public class ExampleActivity extends ProxyActivity {
 
     @Override
     public HLDelegate setRootDelegate() {
-        return new CanlendarDelegateT();
+        return null;
     }
 
 
